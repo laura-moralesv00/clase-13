9c4fd24b78f2728e59e6ed707cedb7084a6920f1
laura-moralesv00 <98362099+laura-moralesv00@users.noreply.github.com>
2022-11-16 18:14:47 -0500

    clase
    clase

--- source/clase-13.R
+++ source/clase-13.R
@@ -19,10 +19,10 @@ my_html <-
 <title> Título de la página: ejemplo de clase </title>
 </head>
 <body>
-<h1> Title 1.</h1>
+<h1> Laura Morales Vernaza.</h1>
 <h2> Subtitle <u>subrayado-1</u>. </h2>
-<p> Este es un párrafo muy pequeño que se encuentra dentro de la etiqueta <b>p</b> de <i>html</i> </p>
-<a href="https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol"> link a wikipedia </a>
+<p> Yo escribí este párrafo muy pequeño que se encuentra dentro de la etiqueta <b>p</b> de <i>html</i> </p>
+<a href="https://en.wikipedia.org/wiki/FIFA"> link a wikipedia </a>
 </body>
 </html>'
 
@@ -34,14 +34,14 @@ browseURL("my_page.html") ## leer con el navegador de su equipo
 vignette("rvest")
 
 ## 2.1 Aplicación en R:
-my_url = "https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol"
+my_url = "https://en.wikipedia.org/wiki/FIFA"
 browseURL(my_url) ## Ir a la página
 
 ## leer el html de la página
-
+my_html <- read_html(my_url)
 
 ## ver la clase del objeto
-
+class(my_html)
 
 ## ver el objeto
 
@@ -50,15 +50,19 @@ browseURL(my_url) ## Ir a la página
 
 
 ## Obtener los elementos h2 de la página
-
-
-## Ver los textos
-
+my_html %>% html_nodes("h2") %>% html_text()
 
 ## 2.3 Xpath
-
-
+html_p <- my_html %>% html_nodes(xpath='//*[@id="mw-content-text"]/div[1]/p[6]')
+html_p %>% html_text()
+  
 ## extraer todas las tablas del html 
+html_a <- html_p %>% html_elements("a")
+html_a
+
+html_a %>% html_attr("href")
+
+ref_tabla_7 <- paste0("https://en.wikipedia.org", html_a %>% html_attr("href"))
 
 
 ## numero de tablas extraidas
